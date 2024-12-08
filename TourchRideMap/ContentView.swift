e47bc076966fdbb28a4855a51225f42248933af9
Nikunj Thakur <nikunj.thakur@thescore.com>
2024-12-08 15:09:22 -0500

    (refactor): Add MapViewModel to implement MVVM architecture for managing map region state

--- TourchRideMap/ContentView.swift
+++ TourchRideMap/ContentView.swift
@@ -9,17 +9,13 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 29.739017, longitude: -95.774269), // TourchRide HQ
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-    )
+    @StateObject private var viewModel = MapViewModel()
 
     var body: some View {
-        Map(coordinateRegion: $region)
-            .edgesIgnoringSafeArea(.all) // Makes the map full screen
+        Map(coordinateRegion: $viewModel.region)
+            .edgesIgnoringSafeArea(.all)
     }
 }
-
 
 #Preview {
     ContentView()
--- TourchRideMap/MapViewModel.swift
+++ TourchRideMap/MapViewModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import MapKit
+
+class MapViewModel: ObservableObject {
+    @Published var region: MKCoordinateRegion
+
+    init() {
+        // Default region (TourchRide HQ)
+        self.region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 29.739017, longitude: -95.774269),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    }
+
+    func updateRegion(latitude: Double, longitude: Double) {
+        region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    }
+}
+
